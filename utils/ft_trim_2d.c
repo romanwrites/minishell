964c3077efb112447c35017a858ce9890a7e0a42
Roman <thekukinpower@gmail.com>
2020-09-30 01:05:57 +0300

    add ft_trim2d function

---
+++ utils/ft_trim_2d.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_trim_2d.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkristie <kukinpower@ya.ru>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/30 00:51:06 by mkristie          #+#    #+#             */
+/*   Updated: 2020/09/30 00:51:08 by mkristie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void		ft_trim_2d(char ***arr_2d)
+{
+	char	**tmp_2d;
+	char 	*tmp;
+	int 	i;
+
+	tmp_2d = *(arr_2d);
+	tmp = NULL;
+	i = 0;
+	while (tmp_2d[i])
+	{
+		tmp = tmp_2d[i];
+		tmp_2d[i] = ft_strtrim(tmp, " \t");
+		free(tmp);
+		tmp = NULL;
+		i++;
+	}
+}
