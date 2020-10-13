e74c76c3338c56b7cdd12554b5295e0d72681be9
Roman <thekukinpower@gmail.com>
2020-10-13 17:39:39 +0300

    handle quotes with env

--- parser/parse.c
+++ parser/parse.c
@@ -70,11 +70,12 @@ _Bool   is_bad_syntax(char c)
 int			get_env_from_str(const char *str)
 {
 	int		i;
+	char    tab[] = {47, 92, 36, 34, 39, '\0'};
 
 	i = 1;
 	while (str[i])
 	{
-		if (str[i] == '/' || str[i] == '$')
+		if (ft_strchr(tab, str[i]) != NULL)
 			break ;
 		i++;
 	}
@@ -105,6 +106,7 @@ void		handle_env(t_mshell *sv, char **str)
 	char	*new_str;
 	size_t		i;
     size_t		j;
+    size_t		save;
 	_Bool	is_alloc;
 	char	*env_value;
 	char	*tmp_env_val;
@@ -114,9 +116,11 @@ void		handle_env(t_mshell *sv, char **str)
 
 	i = 0;
 	j = 0;
+	save = 0;
 	is_alloc = 0;
     env_value = NULL;
 	new_str = ft_strdup("");
+	value_to_check = NULL;
 	ft_alloc_check(new_str);
 	if (str)
 	{
@@ -125,9 +129,13 @@ void		handle_env(t_mshell *sv, char **str)
 		{
 			if (ptr[i] == '$')
 			{
-			    if (i > ft_strlen(new_str) + 1)
+			    if (i > 0 && ptr[i - 1] == 92)
                 {
-			        append_this = ft_substr(ptr, ft_strlen(new_str), i - (ft_strlen(new_str) + 1));
+
+                }
+			    if (i > save + 1)
+                {
+			        append_this = ft_substr(ptr, save, i - save);
                     append_line(&new_str, &append_this);
                 }
                 j = get_env_from_str(ptr + i);
@@ -140,15 +148,45 @@ void		handle_env(t_mshell *sv, char **str)
                 if (!env_value)
                     append_this = ft_strdup("");
                 else
-                    append_this = env_value;
+                {
+                    append_this = ft_strdup(env_value);
+//                    append_this = env_value;
+                }
                 append_line(&new_str, &append_this);
                 i += j - 1;
+                save = i + 1;
             }
 			i++;
 		}
+        if (i > save + 1)
+        {
+            append_this = ft_substr(ptr, save, i - save);
+            append_line(&new_str, &append_this);
+        }
 		free(*(str));
 		*(str) = new_str;
 	}
+}
+
+int     count_chr(const char *str, char c, _Bool pos)
+{
+    size_t i;
+
+    if (!pos)
+    {
+        i = 0;
+        while (str[i] && str[i] == c)
+            i++;
+
+    }
+    else
+    {
+        i = ft_strlen(str) - 1;
+        while (str[i] && str[i] == c)
+            i--;
+        i = ft_strlen(str) - 1 - i;
+    }
+    return (i);
 }
 
 void		parse_env(t_mshell *sv)
@@ -156,6 +194,7 @@ void		parse_env(t_mshell *sv)
 	t_dlist *dlst;
 	char	**ptr;
 	int 	i;
+	char    *tmp;
 
 	i = 0;
 	dlst = sv->dlst_head;
@@ -164,7 +203,35 @@ void		parse_env(t_mshell *sv)
 		ptr = (char **)dlst->content;
 		while (ptr && ptr[i])
 		{
-			if (ft_strchr(ptr[i], (int)'$'))
+		    if (ptr[i][0] == 39)
+		    {
+                tmp = ptr[i];
+                if (count_chr(ptr[i], 39, 0) != count_chr(ptr[i], 39, 1)) {
+                    exit_error_message("syntax error, singly quotes are not closed. parse_env()");
+                }
+                if (count_chr(ptr[i], 39, 0) % 2 == 1) {
+                    ptr[i] = ft_strtrim(tmp, "'");
+                    i++;
+                    continue;
+                } else {
+                    ptr[i] = ft_strtrim(tmp, "'");
+                }
+                free(tmp);
+                tmp = NULL;
+            }
+		    else if (ptr[i][0] == 34)
+            {
+                tmp = ptr[i];
+                if (count_chr(ptr[i], 34, 0) != count_chr(ptr[i], 34, 1))
+                {
+                    exit_error_message("syntax error, singly quotes are not closed. parse_env()");
+                }
+                ptr[i] = ft_strtrim(tmp, "\"");
+                free(tmp);
+                tmp = NULL;
+            }
+//		    if (ptr[i][0] == )
+            if (ft_strchr(ptr[i], (int)'$'))
 				handle_env(sv, &ptr[i]);
 			i++;
 		}
