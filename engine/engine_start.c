6765226626b4604ff276a85e871f904e21535717
Il'q Sagajdak <lhelper@ox-s3.msk.21-school.ru>
2020-10-14 16:45:54 +0300

    init commit after merge

--- engine/engine_start.c
+++ engine/engine_start.c
@@ -6,12 +6,15 @@
 /*   By: lhelper <lhelper@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/25 19:39:20 by mkristie          #+#    #+#             */
-/*   Updated: 2020/10/14 16:11:15 by lhelper          ###   ########.fr       */
+/*   Updated: 2020/10/14 16:45:07 by lhelper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include <fcntl.h>
+
+t_list	*g_env;
+char	*input;
+pid_t	pid;
 
 void	state_bzero(t_parse *state)
 {
@@ -30,6 +33,46 @@ void	init(t_mshell	*sv)
 	state_bzero(sv->state);
 }
 
+void ignore()
+{
+	write(0, "\b\b  \b\b", 6);
+}
+
+void new_line()
+{
+	write(0, "\b\b  \b\b", 6);
+	write(0, "\nzaebash-3.2$ ", ft_strlen("\nzaebash-3.2$ "));
+}
+
+//////////
+void ft_test(char *str)
+{
+	if (*str == '\0')
+		return ;
+	char **cmd = ft_split(str, ' ');
+	if (!(strcmp(cmd[0], "export")))
+		ft_export(cmd[1]);
+	else if (!(strcmp(cmd[0], "env")))
+		ft_env();
+	else if (!(strcmp(cmd[0], "pwd")))
+		ft_pwd();
+	else if (!(strcmp(cmd[0], "echo")))
+	{
+		if (cmd[1])
+			ft_echo(cmd[1], 0);
+	}
+	else if (!(strcmp(cmd[0], "exit")))
+		ft_exit(0);
+	else if (!(strcmp(cmd[0], "cd")))
+	{
+		if (cmd[1])
+			ft_cd(cmd[1]);
+	}
+	else if (!(strcmp(cmd[0], "unset")))
+		ft_unset(cmd[1]);
+}
+//////////
+
 int     main(int ac, char **av, char **envp)
 {
 	t_mshell	*sv;
@@ -37,24 +80,37 @@ int     main(int ac, char **av, char **envp)
 	(void)ac;
 	(void)av;
 
+	signal(SIGQUIT, ignore);
+	signal(SIGINT, new_line);
+	g_env = env_to_list(envp);
 	sv = (t_mshell *)malloc(sizeof(t_mshell));
 	ft_alloc_check(sv);
 	init(sv);
-	sv->envp_mshell = (void *)envp;
+	//sv->envp_mshell = (void *)envp;
 
 	int fd = open("parse_tests.txt", O_RDONLY);
+	write(0, "zaebash-3.2$ ", ft_strlen("zaebash-3.2$ "));
 	while (get_next_line(0, &sv->content))
 	{
-		ft_alloc_check(sv->content);
-		parse_input(sv);
+		ft_test(sv->content);//
+		write(0, "zaebash-3.2$ ", ft_strlen("zaebash-3.2$ "));
+		//ft_alloc_check(sv->content);
+		//parse_input(sv);
+		//t_dlst *tmp = sv->dlst_head
+		//char **ptr = (char **)(sv->dlst_head)->content;
+		//tmp = tmp->next;
+		//print_2d_array(ptr);
 		free(sv->content);
 		sv->content = NULL;
 	}
-	parse_input(sv);
-	if (sv->content)
-	    free(sv->content);
-	sv->content = NULL;
-	free(sv);
-	sv = NULL;
-	sleep(30);
+	if (*(sv->content) == '\0')
+		write(0, "exit\n", ft_strlen("exit\n"));
+	return (0);
+	//parse_input(sv);
+	//if (sv->content)
+	//   free(sv->content);
+	//sv->content = NULL;
+	//free(sv);
+	//sv = NULL;
+	//sleep(30);
 }
