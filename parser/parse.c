7dd7cb747a1fb53c4d612b9fee00db40fd8cc1aa
Roman <thekukinpower@gmail.com>
2020-10-14 18:11:46 +0300

    clear comments

--- parser/parse.c
+++ parser/parse.c
@@ -280,7 +280,6 @@ void		parse_env(t_mshell *sv)
                 free(tmp);
                 tmp = NULL;
             }
-//		    if (ptr[i][0] == )
             if (ft_strchr(ptr[i], (int)'$'))
 				handle_env(sv, &ptr[i]);
 			i++;
@@ -292,27 +291,18 @@ void		parse_env(t_mshell *sv)
 
 void		parse_input(t_mshell *sv)
 {
-	char	*tmp;
 	char	**semicolons2d;
 	static int case_num;
-	case_num++;
-//	split_sh(sv);  Make one function to split elegantly
-
 	char	*input_str = ft_strtrim(sv->content, " \t");
+
+	case_num++;
 	ft_alloc_check(input_str);
-//	printf("\ncase: %d, str: [%s]\nafter split by semicolons:\n", case_num, input_str);
     check_common(input_str);
 	semicolons2d = split_by_char(sv, ';', input_str);
 	ft_trim_2d(&semicolons2d);
-
-//	print_2d_array(semicolons2d);
-//    printf("\nDONE\n\nnext split by pipes:\n\n");
-
 	t_dlist	*dlst;
-
 	char	**tmp_ptr2d;
 	int j = 0;
-//    get_envar(sv->envp_mshell, "PATH"
 	dlst = ft_dlstnew(NULL, NULL);
 	ft_alloc_check(dlst);
     sv->dlst_head = dlst;
@@ -328,7 +318,6 @@ void		parse_input(t_mshell *sv)
         ft_trim_2d(&ptr);
         if (count_2d_lines(ptr) == 1 && is_bad_syntax(ptr[0][ft_strlen(ptr[0]) - 1]))
             exit_error_message("bad syntax");
-//		print_2d_array((char **)dlst->content); //debug print
 		dlst->next = ft_dlstnew(NULL, NULL);
 		ft_alloc_check(dlst->next);
 		dlst = dlst->next;
