e24d1ee76be3481051854e9d16b1b083f058a8e3
Il'q Sagajdak <lhelper@at-l1.msk.21-school.ru>
2020-10-17 16:21:02 +0300

    before merge

--- engine/engine_start.c
+++ engine/engine_start.c
@@ -6,17 +6,37 @@
 /*   By: lhelper <lhelper@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/25 19:39:20 by mkristie          #+#    #+#             */
-/*   Updated: 2020/10/17 16:14:25 by lhelper          ###   ########.fr       */
+/*   Updated: 2020/10/17 16:20:28 by lhelper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "minishell.h"
-#include "../includes/minishell.h"
+#include "minishell.h"
+
+_Bool	g_dquote;
+_Bool	g_squote;
+_Bool	g_backslash;
+int		g_backslash_time;
 
 t_list	*g_env;
 char	*input;
-char	*g_home;
-pid_t	g_pid;
+pid_t	pid;
+
+void	state_bzero(t_parse *state)
+{
+	state->is_double_quote_open = 0;
+	state->is_single_quote_open = 0;
+	state->backslash = 0;
+	state->backslash_time = 0;
+}
+
+void	init(t_mshell	*sv)
+{
+	sv->content = NULL;
+	sv->i = 0;
+	sv->state = (t_parse *)malloc(sizeof(t_parse) * 1); // free after parse
+	ft_alloc_check(sv->state);
+	init_globs();
+}
 
 void ignore()
 {
@@ -25,16 +45,9 @@ void ignore()
 
 void new_line()
 {
-	if (input)
-	{
-		free(input);
-		input = NULL;
-	}
 	write(0, "\b\b  \b\b", 6);
-	write(0, "\nminishell-3.2$ ", ft_strlen("\nminishell-3.2$ "));
+	write(0, "\nzaebash-3.2$ ", ft_strlen("\nzaebash-3.2$ "));
 }
-
-//////////
 
 void ft_test(char *str)
 {
@@ -55,34 +68,31 @@ void ft_test(char *str)
 	else if (!(strcmp(cmd[0], "exit")))
 		ft_exit(0);
 	else if (!(strcmp(cmd[0], "cd")))
-		ft_cd(cmd[1]);
+	{
+		if (cmd[1])
+			ft_cd(cmd[1]);
+	}
 	else if (!(strcmp(cmd[0], "unset")))
 		ft_unset(cmd[1]);
-	else
-		handle_cmd(str);
-}
-//////////
-
-void	state_bzero(t_parse *state)
-{
-	state->is_double_quote_open = 0;
-	state->is_single_quote_open = 0;
-	state->backslash = 0;
-	state->backslash_time = 0;
 }
 
-void	init(t_mshell	*sv)
+_Bool						check_quotes_state(t_parse *state)
 {
-	sv->content = NULL;
-	sv->i = 0;
-	sv->state = (t_parse *)malloc(sizeof(t_parse) * 1); // free after parse
-	ft_alloc_check(sv->state);
-	state_bzero(sv->state);
+	return (!state->is_double_quote_open || !state->is_single_quote_open ? 0 : 1);
 }
+
+//void 						set_quotes_state(t_parse *state)
+//{
+//
+//}
 
 int     main(int ac, char **av, char **envp)
 {
 	t_mshell	*sv;
+	int 		read_res;
+	char		*line;
+
+	line = NULL;
 
 	(void)ac;
 	(void)av;
@@ -90,35 +100,25 @@ int     main(int ac, char **av, char **envp)
 	signal(SIGQUIT, ignore);
 	signal(SIGINT, new_line);
 	g_env = env_to_list(envp);
-	g_home = get_envar("HOME");
 	sv = (t_mshell *)malloc(sizeof(t_mshell));
 	ft_alloc_check(sv);
 	init(sv);
-	//sv->envp_mshell = (void *)envp;
 
-	int fd = open("parse_tests.txt", O_RDONLY);
-	write(0, "minishell-3.2$ ", ft_strlen("minishell-3.2$ "));
+//	int fd = open("parse_tests.txt", O_RDONLY);
+	write(0, PROMPT, ft_strlen(PROMPT));
+
 	while (get_next_line(0, &sv->content))
 	{
-		ft_test(sv->content);//
-		write(0, "minishell-3.2$ ", ft_strlen("minishell-3.2$ "));
-		//ft_alloc_check(sv->content);
-		//parse_input(sv);
-		//t_dlst *tmp = sv->dlst_head
-		//char **ptr = (char **)(sv->dlst_head)->content;
-		//tmp = tmp->next;
-		//print_2d_array(ptr);
+		ft_alloc_check(sv->content);
+		parse_input(sv);
+		t_dlist *tmp = sv->dlst_head;
+		char **ptr = (char **)(sv->dlst_head)->content;
+		print_2d_array(ptr);
 		free(sv->content);
 		sv->content = NULL;
 	}
+
 	if (*(sv->content) == '\0')
 		write(0, "exit\n", ft_strlen("exit\n"));
 	return (0);
-	//parse_input(sv);
-	//if (sv->content)
-	//   free(sv->content);
-	//sv->content = NULL;
-	//free(sv);
-	//sv = NULL;
-	//sleep(30);
 }
