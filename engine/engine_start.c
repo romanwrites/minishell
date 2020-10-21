cfb946b099bf74c9b9b5315e8deda6f0281e1d26
Il'q Sagajdak <lhelper@at-d5.msk.21-school.ru>
2020-10-21 16:57:37 +0300

    pre-merge

--- engine/engine_start.c
+++ engine/engine_start.c
@@ -6,7 +6,7 @@
 /*   By: lhelper <lhelper@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/25 19:39:20 by mkristie          #+#    #+#             */
-/*   Updated: 2020/10/19 19:25:56 by lhelper          ###   ########.fr       */
+/*   Updated: 2020/10/21 16:57:14 by lhelper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,11 @@ _Bool	g_dquote;
 _Bool	g_squote;
 _Bool	g_backslash;
 int		g_backslash_time;
+t_mshell *g_sv;
 
 t_list	*g_env;
 char	*input;
 char	*g_home;
-//int		g_stdin;
-//int		g_stdout;
 pid_t	g_pid;
 
 void	state_bzero(t_parse *state)
@@ -36,7 +35,7 @@ void	init(t_mshell	*sv)
 {
 	sv->content = NULL;
 	sv->i = 0;
-	sv->state = (t_parse *)malloc(sizeof(t_parse) * 1); // free after parse
+	sv->state = (t_parse *)malloc(sizeof(t_parse) * 1); //todo free after parse
 	ft_alloc_check(sv->state);
 	init_globs();
 }
@@ -54,26 +53,47 @@ void new_line()
 		input = NULL;
 	}
 	write(0, "\b\b  \b\b", 6);
-	write(0, "\n", 1);
-	write(0, PROMPT, ft_strlen(PROMPT));
+	write(0, "\nminishell-3.2$ ", ft_strlen("\nminishell-3.2$ "));
+}
+
+void ft_test(char *str)
+{
+	if (*str == '\0')
+		return ;
+	char **cmd = ft_split(str, ' ');
+	if (!(strcmp(cmd[0], "export")))
+		ft_export(cmd[1]);
+	else if (!(strcmp(cmd[0], "env")))
+		ft_env();
+	else if (!(strcmp(cmd[0], "pwd")))
+		ft_pwd();
+	else if (!(strcmp(cmd[0], "echo")))
+	{
+		if (cmd[1])
+			ft_echo(cmd[1], 0);
+	}
+	else if (!(strcmp(cmd[0], "exit")))
+		ft_exit(0);
+	else if (!(strcmp(cmd[0], "cd")))
+		ft_cd(cmd[1]);
+	else if (!(strcmp(cmd[0], "unset")))
+		ft_unset(cmd[1]);
+	else
+		handle_cmd(str);
+}
+
+void 	execute_command(t_mshell *sv, char **cmd)
+{
+
 }
 
 int     main(int ac, char **av, char **envp)
 {
-	t_mshell	*sv;
 	int 		read_res;
-	char		*line;
-	int			i;
-	int			is_pipe;
 	char		**cmd;
-	int fd[2];
-	int savestdout; 
-	int savestdin; 
-	savestdin = dup(0);
-	savestdout = dup(1);
-	i = 0;
-	is_pipe = 0;
+	char		*str;
 
+	str = NULL;
 	(void)ac;
 	(void)av;
 
@@ -81,64 +101,30 @@ int     main(int ac, char **av, char **envp)
 	signal(SIGINT, new_line);
 	g_env = env_to_list(envp);
 	g_home = get_envar("HOME");
-	//g_stdin = dup(0);
-	//g_stdout = dup(1);
-	sv = (t_mshell *)malloc(sizeof(t_mshell));
-	ft_alloc_check(sv);
-	init(sv);
+	g_sv = (t_mshell *)malloc(sizeof(t_mshell));
+	ft_alloc_check(g_sv);
+	init(g_sv);
 
+	t_token *token;
 //	int fd = open("parse_tests.txt", O_RDONLY);
 	write(0, PROMPT, ft_strlen(PROMPT));
 
-	while (get_next_line(0, &sv->content))
+	while (get_next_line(0, &str))
 	{
-		ft_alloc_check(sv->content);
-
-		parse_input(sv);
-		t_dlist *tmp = sv->dlst_head;
-
-		while (tmp) // maybe bad listing, check
-		{
-			cmd = (char **)(sv->dlst_head)->content;
-			if (!cmd)
-				break ;
-			open_quotes_2d(sv, &cmd);
-			if(!is_pipe)
-				execute_command(sv, cmd);
-			else//
-			{
-				pipe(fd);
-				g_pid = fork();
-				if (g_pid == 0)
-				{
-					if (!(i%2))
-					{
-						close(fd[0]);
-						dup2(fd[1], 1);
-						close(fd[1]);
-						execute_command(sv, cmd);
-					}
-					return (0);
-				}
-				else
-				{
-					wait(NULL);
-					if(!(i%2))
-					{
-						close(fd[1]);
-						dup2(fd[0], 0);
-						close(fd[0]);
-					}
-				}
-				i++;
-			}//
-			tmp = tmp->next;
-		}
 		write(0, PROMPT, ft_strlen(PROMPT));
-		free(sv->content);
-		sv->content = NULL;
+		ft_alloc_check(str);
+		if (parse_input(str))
+		{
+			free(str);
+			str = NULL;
+			continue;
+		}
+		print_everything();
+		free(str);
+		str = NULL;
 	}
-	if (*(sv->content) == '\0')
+
+	if (*(str) == '\0')
 		write(0, "exit\n", ft_strlen("exit\n"));
 	return (0);
 }
