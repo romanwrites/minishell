256bedc01dbdb33676333218edb707f2f8deb9fe
Il'q Sagajdak <lhelper@ga-f1.msk.21-school.ru>
2020-10-28 19:17:40 +0300

    stash

--- engine/process.c
+++ engine/process.c
@@ -6,7 +6,7 @@
 /*   By: lhelper <lhelper@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 17:30:21 by lhelper           #+#    #+#             */
-/*   Updated: 2020/10/28 18:18:31 by lhelper          ###   ########.fr       */
+/*   Updated: 2020/10/28 19:15:41 by lhelper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -129,9 +129,9 @@ void		process_cmd(t_mshell *sv)
 				else if (fd == -1)
 				{
 					execute_command(cmd, last_redir, fd, filedes);
-					dup2(savestdin, 0);
-					dup2(savestdout, 1);
-				}//////////////////////////////////////////////////////////////////
+					//dup2(savestdin, 0);
+					//dup2(savestdout, 1);
+				}
 			}
 			token = sv->sh->tdlst_pipe->token_head;
 			sv->sh->tdlst_pipe = sv->sh->tdlst_pipe->next;
