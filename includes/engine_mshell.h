b160aea49e49422cd8e99d384b29711dc958d780
Roman <thekukinpower@gmail.com>
2020-10-28 21:50:38 +0300

    comment print engine_start

--- includes/engine_mshell.h
+++ includes/engine_mshell.h
@@ -14,9 +14,6 @@
 # define ENGINE_MSHELL_H
 #include "structs_mshell.h"
 
-void	state_bzero(t_parse *state);
-_Bool	check_quotes_state(t_parse *state);
-int		gnl_minishell(int fd, char **line, char *shell_str, _Bool (*check_quotes_state)(t_parse *state));
 void	process_cmd(t_mshell *sv);
 
 #endif
