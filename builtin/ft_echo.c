8aa79a55b4498ddf5ee18907a48584125154911d
Il'q Sagajdak <lhelper@su-f1.msk.21-school.ru>
2020-11-02 19:49:09 +0300

    echo

--- builtin/ft_echo.c
+++ builtin/ft_echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkristie <mkristie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lhelper <lhelper@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 16:28:24 by mkristie          #+#    #+#             */
-/*   Updated: 2020/10/30 16:28:25 by mkristie         ###   ########.fr       */
+/*   Updated: 2020/11/02 19:44:01 by lhelper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,26 +24,42 @@ static _Bool	put_g_exit(void)
 
 static void		process_with_flag(char **cmd, int i)
 {
-	i++;
-	if (!(ft_strcmp(cmd[i], "$?")) && put_g_exit())
-		return ;
-	while (cmd[i])
+	if (cmd[i])
 	{
-		write(1, cmd[i], ft_strlen(cmd[i]));
-		if (cmd[i + 1])
-			write(1, " ", 1);
-		i++;
+		if (!(ft_strcmp(cmd[i], "$?")) && put_g_exit())
+			return ;
+		while (cmd[i])
+		{
+			write(1, cmd[i], ft_strlen(cmd[i]));
+			if (cmd[i + 1] && (cmd[i + 1][0] == 0 || cmd[i][0] == 0))
+			{
+				i++;
+				continue;
+			}
+			if (cmd[i + 1] && cmd[i + 1][0] != 0 && cmd[i][0] != 0)
+				write(1, " ", 1);
+			i++;
+		}
 	}
+	else
+		write(1, "\n", 1);
 }
 
 void			ft_echo(char **cmd)
 {
 	int			i;
+	int			is_n;
 
 	i = 1;
+	is_n = 0;
 	if (cmd[i])
 	{
-		if (!(ft_strcmp(cmd[i], "-n")))
+		while(cmd[i] && !(ft_strcmp(cmd[i], "-n")))
+		{
+			is_n = 1;
+			i++;
+		}
+		if (is_n)
 			process_with_flag(cmd, i);
 		else
 		{
@@ -52,6 +68,11 @@ void			ft_echo(char **cmd)
 			while (cmd[i])
 			{
 				write(1, cmd[i], ft_strlen(cmd[i]));
+				if ((cmd[i + 1] && cmd[i + 1][0] == 0) || cmd[i][0] == 0)
+				{
+					i++;
+					continue;
+				}
 				if (cmd[i + 1])
 					write(1, " ", 1);
 				else
