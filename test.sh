39fc04c7a8305e0fc04c4787b1b31596a45011e4
Il'q Sagajdak <lhelper@at-d5.msk.21-school.ru>
2020-11-03 14:42:17 +0300

    test

--- test.sh
+++ test.sh
@@ -20,7 +20,7 @@ BOLDCYAN="\033[1m\033[36m"
 BOLDWHITE="\033[1m\033[37m"
 
 # Compile and set executable rights
-#make -C ../ > /dev/null
+make -C ../ > /dev/null
 cp ../minishell .
 #cp cmake-build-debug/minishell .
 
