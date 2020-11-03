08984bc37b4b9d3fbe201dc6a5f7319ee146daac
Il'q Sagajdak <lhelper@su-g1.msk.21-school.ru>
2020-11-03 17:38:09 +0300

    refresh

--- builtin/refresh.c
+++ builtin/refresh.c
@@ -6,7 +6,7 @@
 /*   By: lhelper <lhelper@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/03 12:22:06 by lhelper           #+#    #+#             */
-/*   Updated: 2020/11/03 17:15:24 by lhelper          ###   ########.fr       */
+/*   Updated: 2020/11/03 17:35:00 by lhelper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,54 +61,13 @@ static void	set_pwd(char *old, char **test)
 
 void	refresh_env(void)
 {
-	//t_list	*env;
 	char	*pwd;
 	char	*old;
 	char	*test[] = {"unset", "OLDPWD", NULL};
 
 	pwd = get_envar("PWD");
-	//env = g_env;
+	if (!pwd)
+		pwd = ft_strdup("");
 	set_oldpwd(old, pwd, test);
-	/*
-	while (env)
-	{
-		if (!ft_strcmp(((t_envar *)(env->content))->key, "OLDPWD"))
-		{
-			old = ((t_envar *)(env->content))->value;
-			((t_envar *)(env->content))->value = ft_strdup(pwd);
-			free(old);
-			flag = 1;
-		}
-		env = env->next;
-	}
-	if (!flag)
-	{
-		test[1] = ft_strjoin("OLDPWD=", pwd);
-		ft_export(test);
-		free(test[1]);
-	}
-	free(pwd);
-	*/
-	//env = g_env;
 	set_pwd(old, test);
-	/*
-	while (env)
-	{
-		if (!ft_strcmp(((t_envar *)(env->content))->key, "PWD"))
-		{
-			old = ((t_envar *)(env->content))->value;
-			((t_envar *)(env->content))->value = malloc(PATH_MAX);
-			if (!((t_envar *)(env->content))->value)
-			{
-				printf("malloc failed!\n");
-				free(old);
-				exit(1);
-			}
-			((t_envar *)(env->content))->value = \
-			getcwd(((t_envar *)(env->content))->value, PATH_MAX);
-			free(old);
-		}
-		env = env->next;
-	}
-	*/
 }
